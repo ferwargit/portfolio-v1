ddb9392ed658c15174adc77609d3171bc2dbd9c6
Fernando Warno <ferwar@hotmail.com>
2025-03-26 04:03:29 -0300

    Navbar Movil y Desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,39 @@ export const Navbar = () => {
           <a href="#home" className="font-mono text-xl font-bold text-white">
             fer<span className="text-blue-500">.tech</span>
           </a>
+
+          {/* Movil Menu */}
+          <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
+            &#9776;
+          </div>
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex items-center space-x-8">
+            <a
+              href="#home"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              About
+            </a>
+            <a
+              href="#projects"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </nav>
